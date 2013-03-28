dbd3a21f033fa9a82290533b3dca35df256e4aff
elPostman <trooz1@gmail.com>
2013-03-28 07:27:19 -0500

    Fixed problem in chrome where parallax scrolled up

--- _themes/rikki-tikki/js/parallax.js
+++ _themes/rikki-tikki/js/parallax.js
@@ -7,30 +7,22 @@ $(document).ready(function() {
 			/* Work out mouse position */
 			var offset = $(this).offset();
 			var xPos = e.pageX - offset.left;
-			var yPos = e.pageY - offset.top;
 
 			/* Get percentage positions */
 			var mouseXPercent = Math.round(xPos / $(this).width() * 100);
-			var mouseYPercent = Math.round(yPos / $(this).height() * 100);
 
 			/* Position Each Layer */
 			$(this).children('img.slide').each(
 				function(){
 					var diffX = $('.Parallax').width() - $(this).width();
-					var diffY = $('.Parallax').height() - $(this).height();
 
 					var myX = diffX * (mouseXPercent / 100); //) / 100) / 2;
 
-
-					var myY = diffY * (mouseYPercent / 100);
-
-
 					var cssObj = {
-						'left': myX + 'px',
-						'top': myY + 'px'
+						'left': myX + 'px'
 					};
 					$(this).css(cssObj);
-					$(this).animate({left: myX, top: myY},{duration: 50, queue: false, easing: 'linear'});
+					$(this).animate({left: myX},{duration: 50, queue: false, easing: 'linear'});
 
 				}
 			);
