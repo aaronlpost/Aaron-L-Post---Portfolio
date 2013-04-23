ac30a96ee31b4c5a9f9624b44afa343c10c5c4f4
elPostman <trooz1@gmail.com>
2013-04-23 17:47:36 -0500

    Added some about page flair. So I could see.

--- _themes/rikki-tikki/js/main.js
+++ _themes/rikki-tikki/js/main.js
@@ -19,3 +19,32 @@ $(document).ready(function() {
 });
 */
 
+$(document).ready(function(){
+	// Cache the Window object
+	var $window = $(window);
+
+      $(window).scroll(function() {
+
+		// Scroll the background at var speed
+		// the yPos is a negative value because we're scrolling it UP!								
+		var yPos = $window.scrollTop();
+
+		if (yPos < 150) {
+		// Put together our final background position
+		var coords = (-450 + (3 * yPos)) + 'px';
+		var opac = ((0.0167 * yPos) - 1.5);
+
+		// Move the background
+		$('.img-overlay').css({"top": coords });
+		$('.img-overlay').css({"opacity": opac });
+		}
+
+		else {
+		$('.img-overlay').css({"top":"0" });
+		$('.img-overlay').css({"opacity":"1" });
+		}
+
+}); // window scroll Ends
+
+
+});
